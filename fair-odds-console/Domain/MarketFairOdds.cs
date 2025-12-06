5e23ce1c069a7bc661028c57e11df53a66b7c5d6
Solifas <43576229+Solifas@users.noreply.github.com>
2025-12-06 22:07:48 +0200

    Add fair odds console application

---
+++ fair-odds-console/Domain/MarketFairOdds.cs
@@ -0,0 +1,12 @@
+namespace FairOddsConsole.Domain;
+
+public class MarketFairOdds
+{
+    public double HomeWin { get; set; }
+    public double Draw { get; set; }
+    public double AwayWin { get; set; }
+    public double Under2_5 { get; set; }
+    public double Over2_5 { get; set; }
+    public double BTTS_Yes { get; set; }
+    public double BTTS_No { get; set; }
+}
---
+++ fair-odds-console/Domain/TeamStrength.cs
@@ -0,0 +1,8 @@
+namespace FairOddsConsole.Domain;
+
+public class TeamStrength
+{
+    public string Name { get; set; } = string.Empty;
+    public double AttackStrength { get; set; } = 1.0;
+    public double DefenceStrength { get; set; } = 1.0;
+}
